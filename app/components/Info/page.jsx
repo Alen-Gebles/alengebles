2f1079ea7b41f6f4a02bcb500694837ef428b60e
Zero <alengebles@gmail.com>
2024-09-01 20:15:56 +0200

    started Info part

---
+++ app/components/Info/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import './info.css';
+
+function Info(){
+  return(
+    <>
+    <section className="info-container">
+      
+    </section>
+    </>
+  )
+}
+
+export default Info;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Header from "./components/Header/page";
+import Info from "./components/Info/page"
 
 export default function Home() {
   return(
     <>
     <main className="mainContainer">
-      <Header />
+      <div className="flex flex-row gap-3">
+        <Header />
+        <Info />    
+      </div>
     </main>
     
     </>
