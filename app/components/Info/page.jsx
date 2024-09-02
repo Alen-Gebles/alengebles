ffef47081c98a624bedabf31e2134f7d7a56a2cb
Zero <alengebles@gmail.com>
2024-09-02 11:04:03 +0200

    more stuff

--- app/components/Info/page.jsx
+++ app/components/Info/page.jsx
@@ -5,7 +5,19 @@ function Info(){
   return(
     <>
     <section className="info-container">
-      
+      <h2 className="linkHead">Links</h2>
+      <ul className="linkUl">
+        <li className="link"><a href="">Home</a></li>
+        <li className="link"><a href="">About me</a></li>
+        <li className="link"><a href="">Diary</a></li>
+        <li className="link"><a href="">Blog</a></li>
+        <li className="link"><a href="">Discord</a></li>
+        <li className="link"><a href="">Twitter</a></li>
+        <li className="link"><a href="">Github</a></li>
+        <li className="link"><a href="">Linkedin</a></li>
+        <li className="link"><a href="">Tumbrl</a></li>
+        <li className="link"><a href="">Email</a></li>
+      </ul>
     </section>
     </>
   )
